28b73b94d390fdd93518745f49d5d10aca775301
dysshanks <ryanvdvorst@outlook.com>
2025-03-10 09:53:41 +0100

    quick setup
    just for convenienience

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+$name = "Home";
+?>
+
+<!doctype html>
+<html lang="en">
+
+<?php require_once "../resources/components/head.php"; ?>
+
+<body>
+
+    <?php require_once "../resources/components/header.php"; ?>
+
+    <main>
+
+    </main>
+
+    <?php require_once "../resources/components/footer.php"; ?>
+</body>
+</html>
